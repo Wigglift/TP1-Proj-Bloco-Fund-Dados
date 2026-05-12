2fb0529cd55c5f0988f7af4fd31b1b832d2cbfa5
Isaac <rodriguesisaac523@gmail.com>
2026-05-12 09:24:27 -0300

    created the rest of tarefa class

---
+++ Status.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Status(Enum):
+    PENDENTE = 1
+    EM_ANDAMENTO = 2
+    CONCLUIDO = 3
--- Tarefa.py
+++ Tarefa.py
@@ -1,6 +1,24 @@
+from datetime import date
+from Status import Status
+
 class Tarefa():
-    def __init__(self):
-        pass
+    def __init__(self, id: int, descricao: str, prazo_final: date, urgencia: int):
+        self.id = id
+        self.descricao = descricao
+        self.data_de_criacao = date.today()
+        self.status = Status.PENDENTE
+        self.prazo_final = prazo_final
+        self.urgencia = urgencia
 
     def __str__(self):
-        pass
+        response = (
+            f"Tarefa {self.id}\n"
+            f"Descrição: {self.descricao}\n"
+            f"Status: {self.status.name}\n"
+            f"Data de criação: {self.data_de_criacao.strftime('%d/%m/%Y')}\n"
+            f"Prazo final: {self.prazo_final.strftime('%d/%m/%Y')}\n"
+            f'Urgência: {self.urgencia}"\n'
+    
+        )
+
+        return response
